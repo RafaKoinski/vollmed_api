05d0ce86aece6d3fee161816a88ff2eea6c1790a
Rafael Koinski <rkoinski@gmail.com>
2024-05-20 15:38:55 -0300

    Finalizando o projeto

--- src/main/java/med/voll/api/config/SecurityFilter.java
+++ src/main/java/med/voll/api/config/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,20 +18,30 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private  TokenService tokenService;
+
+    @Autowired
+    private UsuariosRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = recuperaToken(request);
-        var subjeto = tokenService.getSubject(token);
-        System.out.println(subjeto);
+
+        if (token != null){
+            var subjeto = tokenService.getSubject(token);
+            var usuario = repository.findByLogin(subjeto);
+
+            var auth = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String recuperaToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null){
-            throw new RuntimeException("Token não enviado no cabeçalho");
+        if (authHeader != null){
+            return authHeader.replace("Bearer ", "");
         }
-        return authHeader.replace("Bearer ", "");
+        return null;
     }
 }
